69613b07dcc60f9221db086193c4851860e6ecaa
joe <joeldrt@gmail.com>
2018-07-17 03:02:58 -0500

    creation of test action for testing ifttt calls

--- da_ifttt_api.py
+++ da_ifttt_api.py
@@ -1,6 +1,7 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+import datetime
 import logging
 
 app = Flask(__name__, static_url_path='/static')
@@ -17,7 +18,39 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 
+parser = reqparse.RequestParser()
+parser.add_argument('actionFields', type=dict, required=True)
+parser.add_argument('ifttt_source', type=dict, required=True)
+parser.add_argument('user', type=dict)
+
+
+class TestAction(Resource):
+    def post(self):
+        for key, value in request.headers:
+            logging.debug('Headers-------------------')
+            logging.debug('{}: {}'.format(key, value))
+
+        data = parser.parse_args()
+
+        for key, value in data['actionFields']:
+            logging.debug('Action fields ------------')
+            logging.debug('{}: {}'.format(key, value))
+
+        for key, value in data['ifttt_source']:
+            logging.debug('Ifttt source -------------')
+            logging.debug('{}: {}'.format(key, value))
+
+        response = {'id': datetime.datetime.now,
+                    'url': 'http://www.digiall.mx'}
+
+        reasponse_data = list()
+        reasponse_data.append(response)
+
+        return {'data': reasponse_data}
+
+
 api.add_resource(HelloWorld, '/api/helloworld')
+api.add_resource(TestAction, '/api/ifttt/v1/actions/testaction')
 
 if __name__ == '__main__':
     app.run(debug=True)
